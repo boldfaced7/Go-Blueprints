17224765ad560a0fabd61dc6186315d59a24018d
준모 강 <boldfaced7@hanyang.ac.kr>
2022-05-23 13:17:05 +0900

    Added templateHandler

--- chapter1/chat/main.go
+++ chapter1/chat/main.go
@@ -1,23 +1,31 @@
 package main
 
 import (
+	"html/template"
 	"log"
 	"net/http"
+	"path/filepath"
+	"sync"
 )
 
-func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte(`
-		<html>
-			<head>
-				<title>Chat</title>
-			</head>
-			<body>
-				Let's chat!
-			</body>
-		</html>
-		`))
+// templ은 하나의 템플릿을 나타냄
+type templateHandler struct {
+	once     sync.Once
+	filename string
+	templ    *template.Template
+}
+
+// ServeHTTP가 HTTP 요청을 처리
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.once.Do(func() {
+		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
 	})
+	t.templ.Execute(w, nil)
+}
+
+func main() {
+	// 루트
+	http.Handle("/", &templateHandler{filename: "chat.html"})
 
 	// 웹 서버 시작
 	if err := http.ListenAndServe(":8080", nil); err != nil {
