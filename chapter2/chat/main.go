feb322bdb25f1c5dfdaa2447c634a09732951b66
준모 강 <boldfaced7@hanyang.ac.kr>
2022-06-10 13:03:38 +0900

    chapter2 started

---
+++ chapter2/chat/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"flag"
+	"html/template"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
+	"sync"
+
+	"github.com/boldfaced7/Go-Blueprints/chapter1/trace"
+)
+
+// templ은 하나의 템플릿을 나타냄
+type templateHandler struct {
+	once     sync.Once
+	filename string
+	templ    *template.Template
+}
+
+// ServeHTTP가 HTTP 요청을 처리
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.once.Do(func() {
+		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
+	})
+	t.templ.Execute(w, r)
+}
+
+func main() {
+	var addr = flag.String("addr", ":8080", "The addr of the application.")
+	flag.Parse() // 플래그 파싱
+	r := newRoom()
+	r.tracer = trace.New(os.Stdout)
+	http.Handle("/", MustAuth(&templateHandler{filename: "chat.html"}))
+	http.Handle("/room", r)
+	// 방을 가져옴
+	go r.run()
+	// 웹 서버 시작
+	log.Println("Starting web server on", *addr)
+	if err := http.ListenAndServe(*addr, nil); err != nil {
+		log.Fatal("ListenAndServe:", err)
+	}
+}
